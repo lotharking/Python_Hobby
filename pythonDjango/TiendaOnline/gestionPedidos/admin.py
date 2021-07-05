b051653df842376795d519c9e83cab309e58bbb3
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-07-05 13:56:04 -0500

    trabajando con menu administrador

--- pythonDjango/TiendaOnline/gestionPedidos/admin.py
+++ pythonDjango/TiendaOnline/gestionPedidos/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from gestionPedidos.models import Clientes, Articulos, Pedidos
+
 # Register your models here.
+
+class ClientesAdmin(admin.ModelAdmin):
+    list_display=("nombre","direccion","telefono")
+    search_fields=("nombre","telefono")
+
+admin.site.register(Clientes,ClientesAdmin)
+admin.site.register(Articulos)
+admin.site.register(Pedidos)
