4978e875e57110ff7b32920cafe9031d689a9e54
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-07-07 21:48:23 -0500

    insertando items al json

--- PokeApiFlask/API.py
+++ PokeApiFlask/API.py
@@ -2,6 +2,7 @@ from os import name
 import re
 from flask import Flask, json,request
 from flask.helpers import flash
+from numpy import array
 import pandas as pd
 
 def cargar_datos(ruta):
@@ -25,6 +26,17 @@ def obtener_pokemon(name):
     pokemon=cargar_datos(ruta)
     res=pokemon.loc[(pokemon['name']==name)]
     return json.loads(res.to_json(orient='index'))
+
+@app.route('/pokemon/ingresar',methods=['GET','POST'])
+def ingresar_pokemon():
+    pokemon=cargar_datos(ruta)
+    n=pokemon.shape[0]
+    identidad=int(request.args.get('Id'))
+    nombre=request.args.get('Pokemon_nombre')
+    tipo=array(request.args.get('Types').split(','))
+    pokemon.loc[n]=[identidad,nombre,tipo]
+    pokemon.to_json(r'pokedex.json')
+    return 'Exito'
 
 if __name__=="__main__":
     ruta='pokedex.json'
