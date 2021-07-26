1ea70b2bf4c7f506d2d5d3317c445ce8fd45e876
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-07-26 16:54:00 -0500

    añadiendo url

--- pythonDjango/PaginaWeb/ProyectoWebApp/views.py
+++ pythonDjango/PaginaWeb/ProyectoWebApp/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Snicio")
+    return HttpResponse("Home")
 
 def servicios(request):
     return HttpResponse("Servicios")
