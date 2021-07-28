954e9cd138729698f9521b7daf9063971a924435
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-07-27 21:24:12 -0500

    [change] cambiando idioma y añadiendo campos a admin.

--- pythonDjango/PaginaWeb/servicios/admin.py
+++ pythonDjango/PaginaWeb/servicios/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from .models import Servicio
 
 # Register your models here.
 
+# Añadir campos de created y updated para mostrar
+class ServicioAdmin(admin.ModelAdmin):
+    readonly_fields=('created', 'updated')
 
+# Resgistrando el modelo en la parte de admin
+admin.site.register(Servicio, ServicioAdmin)
