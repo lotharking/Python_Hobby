0e4494e04da9292b7a221b165095f275a5b8cc48
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-07-31 14:00:03 -0500

    [change] añadiendo busqueda por categorias

--- pythonDjango/PaginaWeb/blog/views.py
+++ pythonDjango/PaginaWeb/blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from blog.models import post
+from blog.models import post, categoria
 
 # Create your views here.
 
 def blog(request):
     posts=post.objects.all()
     return render(request, "blog/blog.html",{'posts':posts})
+
+def categoriaMethod(request, categoria_id):
+    categ=categoria.objects.get(id=categoria_id)
+    posts=post.objects.filter(categoria=categ)
+    return render(request, "blog/categoria.html",{'categorias':categ, 'posts':posts})
