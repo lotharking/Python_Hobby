c270e3942b74f0ae6235ef2ee3426163076b4a82
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-07-31 14:00:03 -0500

    [change] añadiendo busqueda por categorias

--- pythonDjango/PaginaWeb/blog/urls.py
+++ pythonDjango/PaginaWeb/blog/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.blog, name="Blog"), # El path esta vacio porque esta en la misma carpeta de la vista
+    path('categoria/<int:categoria_id>/', views.categoriaMethod, name="Categoria"), 
 ]
