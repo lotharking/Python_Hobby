735fb4a555a8eb863c87261a20dd5eb1ae8477ab
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-07-31 20:43:54 -0500

    [change] modificacion estilos y formato formularios

--- pythonDjango/PaginaWeb/contacto/forms.py
+++ pythonDjango/PaginaWeb/contacto/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.forms.fields import EmailField
+from django.forms.widgets import Textarea
 
 class FormularioContacto(forms.Form):
     nombre=forms.CharField(label="Nombre", required=True)
     email=forms.CharField(label="Correo", required=True)
-    contenido=forms.CharField(label="Contenido")
+    contenido=forms.CharField(label="Contenido", widget=Textarea)
