e2ddaa8f5898fd931a627a5bfbf6a6a901b40d20
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-08-02 14:14:14 -0500

    [change] Envio de datos del formulario

--- pythonDjango/PaginaWeb/contacto/views.py
+++ pythonDjango/PaginaWeb/contacto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from .forms import FormularioContacto
+from django.core.mail import EmailMessage
 
 # Create your views here.
 
@@ -11,5 +12,15 @@ def Contacto(request):
             nombre=request.POST.get("nombre")
             email=request.POST.get("email")
             contenido=request.POST.get("contenido")
-            return redirect("/contacto/?valido")
+
+            email=EmailMessage("Mensaje de la app", 
+            "El usuario con el nombre {} con la direccion {} escribe lo siguiente: \n\n {}".format(nombre,email,contenido),
+            "", ["andresfelipe083195@gmail.com"],reply_to=[email]) # Direccion a donde va a llegar
+
+            try:
+                email.send()
+                return redirect("/contacto/?valido")
+            except:
+                return redirect("/contacto/?novalido")                
+
     return render(request, "contacto/contacto.html", {'miFormulario': formulario_contacto})
