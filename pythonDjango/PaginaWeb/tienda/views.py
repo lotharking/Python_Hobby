63cc7f272cff8ee286d0939db59b4d5748ebc454
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-08-02 15:47:25 -0500

    [change] Mostrando items en el apartado tienda

--- pythonDjango/PaginaWeb/tienda/views.py
+++ pythonDjango/PaginaWeb/tienda/views.py
@@ -1,6 +1,10 @@
+from .models import Producto
 from django.shortcuts import render
 
 # Create your views here.
 
 def tienda(request):
-    return render(request, "tienda/tienda.html")
+
+    productos= Producto.objects.all()
+
+    return render(request, "tienda/tienda.html", {'productos':productos})
