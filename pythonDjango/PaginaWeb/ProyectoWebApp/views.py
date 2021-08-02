f1652649e963e1ca8ddeef87c16c5d1283ccd353
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-08-02 14:25:48 -0500

    [create] creacion app para la tienda

--- pythonDjango/PaginaWeb/ProyectoWebApp/views.py
+++ pythonDjango/PaginaWeb/ProyectoWebApp/views.py
@@ -5,6 +5,3 @@ from django.shortcuts import render, HttpResponse
 def home(request):
     return render(request, "ProyectoWebApp/home.html")
 
-def tienda(request):
-    return render(request, "ProyectoWebApp/tienda.html")
-
