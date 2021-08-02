0a0f51ca0e101020686bd1495039becc5ee00a43
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-08-01 22:18:59 -0500

    [change] Manejo de GET y POST para manejar informacion

--- pythonDjango/PaginaWeb/contacto/views.py
+++ pythonDjango/PaginaWeb/contacto/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import FormularioContacto
 
 # Create your views here.
 
 def Contacto(request):
     formulario_contacto=FormularioContacto()
+    if request.method=="POST":
+        formulario_contacto=FormularioContacto(data=request.POST)
+        if formulario_contacto.is_valid():
+            nombre=request.POST.get("nombre")
+            email=request.POST.get("email")
+            contenido=request.POST.get("contenido")
+            return redirect("/contacto/?valido")
     return render(request, "contacto/contacto.html", {'miFormulario': formulario_contacto})
