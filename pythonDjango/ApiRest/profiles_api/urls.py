d76da4a323c596006f988ffdb7a696642c2a08b9
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-08-07 14:46:33 -0500

    [change] creacion API para manejo de perfiles de usuario

--- pythonDjango/ApiRest/profiles_api/urls.py
+++ pythonDjango/ApiRest/profiles_api/urls.py
@@ -6,6 +6,7 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
+router.register('profile', views.UserProfileViewSet) # Como en la vista se usa el queryset evita el uso de basename
 
 urlpatterns = [
     path('hello-view', views.HelloApiView.as_view()),
