4dcaad209c4d86bf3066b7b137e483377bd8f478
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-08-07 12:57:34 -0500

    [change] Viendo los diferentes usos para los APIView y ViewSet

--- pythonDjango/ApiRest/profiles_api/views.py
+++ pythonDjango/ApiRest/profiles_api/views.py
@@ -1,13 +1,20 @@
 import re
 from django.http import response
+from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
 
 from profiles_api import serializers
 
 class HelloApiView(APIView):
-    """ Clase APIView prueba """
+    """ Clase APIView prueba con sus tipos de metodos existentes para API's
+    Usados cuando: 
+    - Control logica
+    - Proceso de archivos y renderizado de respuesta sincrona
+    - Lamadas API's y servicios
+    - Acceso a archivos locales o datos """
+
     serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
@@ -32,3 +39,68 @@ class HelloApiView(APIView):
             return Response({'message':message})
         else:
             return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """ Maneja actualizacion de un objeto, para actualizar objeto se le pide ID
+         pero como en este caso no se trabajara con ID se define el 'pk=None' """
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """ Maneja actualizacion parcial de un objeto """
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """ Maneja eliminacion de un objeto """
+        return Response({'method': 'DELETE'})
+
+class HelloViewSet(viewsets.ViewSet):
+    """ ViewSet de prueba, usado para editar objetos en especifico y
+    es por ello que es el mas comun en las DB """
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """ Retornar mensaje en hola mundo """
+
+        a_viewset = [
+            'usa acciones (list, create, update, partial_update)',
+            'Automaticamente mapea a los URLs usando Routers',
+            'Proveee mas funcionalidades con menos codigo'
+        ]
+
+        return Response({'message': 'Hola', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """ Crear nuevo mensaje de Hola Mundo """
+
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"Hola {name}"
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors, 
+                status = status.HTTP_400_BAD_REQUEST
+                )
+
+    def retrieve(self, request, pk=None):
+        """ Obtiene un objeto y su ID """
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """ Actualiza un objeto """
+
+        return Response({'http_method': 'PUT'})
+
+    def parcial_update(self, request, pk=None):
+        """ Actualiza un objeto con informacion parcial """
+
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """ Destruye un objeto """
+
+        return Response({'http_method': 'DELETE'})
