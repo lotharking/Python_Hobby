895c7095a8d6bfb4457b6e2f8d1632425b62cf35
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-08-16 16:37:35 -0500

    [change] test para usuarios publicos y para usuario ya existente

---
+++ pythonDjango/ApiRestAvanzado/user/tests/test_user_api.py
@@ -0,0 +1,48 @@
+from _typeshed import Self
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status # Ayudan a poner los mensajes de error como se conocen con 200-400-500
+
+CREATE_USER_URL = reverse('user:create')
+
+def create_user(**params): # Para pasarle la cantidad de argumentos que se desee
+    return get_user_model().objects.create_user(**params)
+
+class PublicUserApiTest(TestCase): # Test publicos(se separa el tipo de usuarios)
+    """ Testear el api para usuarios publicos """
+    def setUp(self):        
+        """ Primera funcion para definir el APIClient"""
+        Self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        """ Probar crear usuario con un payload exitoso """
+        payload = {
+            'email': 'test@test.com',
+            'password': 'testpassword',
+            'name': 'Test name'
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        user = get_user_model().objects.get(**res.data) # Con esto obtenemos los datos del cliente -- **res.data
+
+        self.assertTrue(user.check_password(payload['password']))
+
+        self.assertNotIn('password', res.data)
+
+    def test_user_exist(self):
+        -""" probar crear un usuario que ya existe y falla """
+        payload = {
+            'email': 'test@test.com',
+            'password': 'testpassword'
+        }
+        create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
