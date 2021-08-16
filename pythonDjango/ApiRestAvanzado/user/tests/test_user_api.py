41f8241c23cfc31731e6537d8d7e58cd3f3d1848
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-08-16 16:49:52 -0500

    [change] Añadiendo test contraseña corta

--- pythonDjango/ApiRestAvanzado/user/tests/test_user_api.py
+++ pythonDjango/ApiRestAvanzado/user/tests/test_user_api.py
@@ -1,4 +1,5 @@
 from _typeshed import Self
+from django.forms.fields import EmailField
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -33,7 +34,7 @@ class PublicUserApiTest(TestCase): # Test publicos(se separa el tipo de usuarios
 
         self.assertTrue(user.check_password(payload['password']))
 
-        self.assertNotIn('password', res.data)
+        self.assertNotIn('password', res.data) # Asegurarse que nos e pase la contraseña
 
     def test_user_exist(self):
         -""" probar crear un usuario que ya existe y falla """
@@ -46,3 +47,20 @@ class PublicUserApiTest(TestCase): # Test publicos(se separa el tipo de usuarios
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short(self):
+        """ Validar que la contraseña no es muy corta """
+        payload = {
+            'email': 'test@test.com',
+            'password': 'pw'
+        }
+
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+        user_exist = get_user_model().objects.filter(
+            email = payload['email']
+        ).exists()
+
+        self.assertFalse(user_exist)
+
