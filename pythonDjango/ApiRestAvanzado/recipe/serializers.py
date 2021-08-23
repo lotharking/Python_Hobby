02aeda13c5f5057186a46e8748ab8a3739e01c4e
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-08-23 11:51:57 -0500

    [change] Añadiendo test para enlistar ingredientes

--- pythonDjango/ApiRestAvanzado/recipe/serializers.py
+++ pythonDjango/ApiRestAvanzado/recipe/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-from core.models import Tag
+from core.models import Ingredient, Tag
 
 class TagSerializer(serializers.ModelSerializer):
     """ Serializador para objeto de Tag """
     class Meta:
         model = Tag
+        fields = ('id', 'name')
+        read_only_Fields = ('id',)
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """ Serializador para objeto de Tag """
+    class Meta:
+        model = Ingredient
         fields = ('id', 'name')
         read_only_Fields = ('id',)
