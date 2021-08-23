93d5992e44c836553e194797aef9977fbfa89d7d
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-08-23 12:11:31 -0500

    [change] Añadiendo test para crear ingredientes

--- pythonDjango/ApiRestAvanzado/recipe/tests/test_ingredients_api.py
+++ pythonDjango/ApiRestAvanzado/recipe/tests/test_ingredients_api.py
@@ -64,3 +64,24 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data),1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
+        
+    def test_create_ingredient_successful(self):
+        """ Prueba creando nuevo ingrediente """
+        payload = {'name': 'Chocolate'}
+
+        self.client.post(INGREDIENT_URL,payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_ingrediente_invalid(self):
+        """ Prueba crear un nuevo ingrediente con un payload invalido """
+        payload = {'name': ''}
+
+        res = self.client.post(INGREDIENT_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
