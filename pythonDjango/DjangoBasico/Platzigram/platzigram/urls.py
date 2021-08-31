d54f5229749f53a271b6930f1a8b2058ac49e7f1
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-08-30 20:20:33 -0500

    [create] Implementando modelo de post

--- pythonDjango/DjangoBasico/Platzigram/platzigram/urls.py
+++ pythonDjango/DjangoBasico/Platzigram/platzigram/urls.py
@@ -1,19 +1,9 @@
-"""platzigram URL Configuration
+"""Platzi url module"""
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# Django
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from platzigram import view as local_view
@@ -26,4 +16,4 @@ urlpatterns = [
     path('hi/<str:name>/<int:age>/', local_view.say_hi),
 
     path('posts/', posts_views.list_posts),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
