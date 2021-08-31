6cf7c5d45976e6ecf0e47c901f074bd4b40964f8
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-08-30 19:27:48 -0500

    [change] Editando la informacion a mostrar en el admin

--- pythonDjango/DjangoBasico/Platzigram/user/admin.py
+++ pythonDjango/DjangoBasico/Platzigram/user/admin.py
@@ -1,9 +1,11 @@
 """User admin class"""
 
 # Django
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
 
 # Models
+from django.contrib.auth.models import User
 from user.models import Profile
 
 @admin.register(Profile)
@@ -27,3 +29,41 @@ class ProfileAdmin(admin.ModelAdmin):
         'created', 
         'modified',
         )
+
+    fieldsets = (
+        ('Profile', {
+            'fields':(('user', 'picture'),),
+        }),
+        ('Extra info', {
+            'fields':(('wesite', 'phone_number'),
+                    ('biography'),),
+        }),
+        ('Metadata', {
+            'fields':('created', 'modified',),
+        }),
+    )
+
+    readonly_fields = ('created', 'modified')
+
+class ProfileInline(admin.StackedInline):
+    """Profile admin in.line for user"""
+
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profiles'
+
+class UserAdmin(BaseUserAdmin):
+    """Add profile admin to BaseUserAdmin"""
+
+    inlines = (ProfileInline,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff',
+    )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
