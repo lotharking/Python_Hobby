e78a094dd1d734b2bc42ea9969153de009e958d2
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-09-02 20:34:16 -0500

    [create] Implementando login

--- pythonDjango/DjangoBasico/Platzigram/platzigram/settings.py
+++ pythonDjango/DjangoBasico/Platzigram/platzigram/settings.py
@@ -136,8 +136,9 @@ STATICFILES_FINDERS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / 'media'
-
 MEDIA_URL = '/media/'
+
+LOGIN_URL = '/users/login/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
