671e9ec84e6ea4863cc7f10fc5ccefce2a5a8679
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-09-02 20:34:16 -0500

    [create] Implementando login

--- pythonDjango/DjangoBasico/Platzigram/platzigram/urls.py
+++ pythonDjango/DjangoBasico/Platzigram/platzigram/urls.py
@@ -8,12 +8,15 @@ from django.urls import path
 
 from platzigram import view as local_view
 from posts import views as posts_views
+from user import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello-word/', local_view.hello_word),
-    path('sorted/', local_view.sorted),
-    path('hi/<str:name>/<int:age>/', local_view.say_hi),
+    path('hello-word/', local_view.hello_word, name='hello_word'),
+    path('sorted/', local_view.sorted, name= 'sort'),
+    path('hi/<str:name>/<int:age>/', local_view.say_hi, name='hi'),
 
-    path('posts/', posts_views.list_posts),
+    path('posts/', posts_views.list_posts, name='feed'),
+
+    path('user/login/', users_views.login_view, name='login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- pythonDjango/DjangoBasico/Platzigram/user/views.py
+++ pythonDjango/DjangoBasico/Platzigram/user/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+"""Users view"""
 
-# Create your views here.
+# Django
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login
+
+def login_view(request):
+    """Login view"""
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user_log = authenticate(request, username=username, password=password)
+    else:
+        return render(request, 'users/login.html')
+
+    if user_log:
+        login(request, user_log)
+        return redirect('feed')
+    else:
+        return render(request, 'users/login.html', {'error':'Invalid username or password'})
+    return render(request, 'users/login.html')
