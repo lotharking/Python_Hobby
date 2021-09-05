3c7c9a3dd92710c97050d40ae9010f108a6d8a23
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-09-05 17:34:55 -0500

    [create] Implementando singup

--- pythonDjango/DjangoBasico/Platzigram/user/views.py
+++ pythonDjango/DjangoBasico/Platzigram/user/views.py
@@ -5,6 +5,13 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
+# Exception
+from django.db.utils import IntegrityError
+
+# Models
+from django.contrib.auth.models import User
+from user.models import Profile
+
 def login_view(request):
     """Login view"""
     if request.method == 'POST':
@@ -20,6 +27,32 @@ def login_view(request):
     else:
         return render(request, 'users/login.html', {'error':'Invalid username or password'})
     return render(request, 'users/login.html')
+
+
+def singup(request):
+    """singup view"""
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+        if passwd != passwd_confirmation:
+            return render(request, 'users/singup.html', {'error':'Password confirmation does not match'})
+
+        try:
+            user = User.objects.create_user(username=username, password = passwd)
+        except IntegrityError:
+            return render(request, 'users/singup.html', {'error': 'Username is alredy in user'})
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/singup.html')
 
 @login_required
 def logout_view(request):
