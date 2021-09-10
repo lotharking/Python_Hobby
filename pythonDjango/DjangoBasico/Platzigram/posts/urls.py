9fc6b387cac7385accf71088615e52013b6fe140
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-09-09 21:20:41 -0500

    [change] Añadiendo login_required a detail user y implementando vista de listado de posts por clases

--- pythonDjango/DjangoBasico/Platzigram/posts/urls.py
+++ pythonDjango/DjangoBasico/Platzigram/posts/urls.py
@@ -7,9 +7,14 @@ from django.urls import path
 from posts import views
 
 urlpatterns = [
+    # path(
+    #     route='', 
+    #     view=views.PostsFeedView.as_view(), 
+    #     name='feed'
+    #     ),
     path(
         route='', 
-        view=views.list_posts, 
+        view=views.PostsFeedView.as_view(), 
         name='feed'
         ),
     path(
--- pythonDjango/DjangoBasico/Platzigram/posts/views.py
+++ pythonDjango/DjangoBasico/Platzigram/posts/views.py
@@ -1,21 +1,34 @@
 """ Posts views """
 
 # Django
-from posts.models import Posts
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
-
-# Utilities
-from datetime import datetime
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView
 
 # Form
 from posts.forms import PostForm
 
-@login_required
-def list_posts(request):
-    """ List existing post """
-    posts = Posts.objects.all().order_by('-created')
-    return render(request, 'posts/feed.html', {'posts': posts})
+# Models
+from posts.models import Posts
+
+class PostsFeedView(LoginRequiredMixin, ListView):
+    """Return all publish posts"""
+
+    template_name = 'posts/feed.html'
+    model = Posts
+    ordering = ('-created')
+    paginate_by = 2
+    context_object_name = 'posts'
+
+# class DetailFeedView(LoginRequiredMixin, DetailView):
+#     """DetailView of posts"""
+
+#     template_name = 'users/detail.html'
+#     slug_field = 'username'
+#     slug_url_kwarg = 'slug'
+#     queryset = User.objects.all()
+#     context_object_name = 'user'
 
 @login_required
 def create(request):
