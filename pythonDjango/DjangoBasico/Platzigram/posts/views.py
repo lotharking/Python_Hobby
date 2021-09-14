a66ca132c5cae0612542e4f0cb794b1e6d80fe42
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-09-13 19:34:33 -0500

    [change] Añadiendo create view

--- pythonDjango/DjangoBasico/Platzigram/posts/views.py
+++ pythonDjango/DjangoBasico/Platzigram/posts/views.py
@@ -3,8 +3,9 @@
 # Django
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 # Form
 from posts.forms import PostForm
@@ -28,32 +29,16 @@ class PostDetailView(LoginRequiredMixin, DetailView):
     queryset = Posts.objects.all()
     context_object_name = 'post'
 
+class CreatePostView(CreateView):
+    """CreateView Posts"""
+
+    template_name = 'posts/new.html'
+    form_class = PostForm
+    success_url = reverse_lazy('posts:feed')
+
     def get_context_data(self, **kwargs):
-        """Add users posts to context"""
         context = super().get_context_data(**kwargs)
-        user = self.get_object()
-        context['posts'] = Posts.objects.filter(user=user).order_by('-created')
+        context['user'] = self.request.user
+        context['profile'] = self.request.user.profile
         return context
-
-@login_required
-def create(request):
-    """Create new post view"""
-
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('posts:feed')
-    else:
-        form = PostForm()
-
-    return render(
-        request,
-        'posts/new.html',
-        context={
-            'form':form,
-            'user':request.user,
-            'profile':request.user.profile,
-        }
-        )
 
