2a735211ea40460d1df0c04f3d6fcbebed200d21
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-09-14 17:25:18 -0500

    [change] Añadiendo Sign up y logged out basado en clases

--- pythonDjango/DjangoBasico/Platzigram/user/urls.py
+++ pythonDjango/DjangoBasico/Platzigram/user/urls.py
@@ -8,32 +8,32 @@ from user import views
 
 urlpatterns = [
 
+    # Management
+    path(
+        route = 'login/', 
+        view = views.LoginView.as_view(), 
+        name='login'
+    ),
+    path(
+        route = 'logout/', 
+        view = views.LogoutView.as_view(), 
+        name='logout'
+    ),
+    path(
+        route = 'signup/', 
+        view = views.SignupView.as_view(), 
+        name='signup'
+    ),
+    path(
+        route = 'me/profile/', 
+        view = views.UpdateProfileView.as_view(), 
+        name='update'
+    ),
+
     # Posts
     path(
         route = '<str:slug>', 
         view = views.UserDetailView.as_view(), 
         name='detail'
-    ),
-
-    # Management
-    path(
-        route = 'login/', 
-        view = views.login_view, 
-        name='login'
-    ),
-    path(
-        route = 'logout/', 
-        view = views.logout_view, 
-        name='logout'
-    ),
-    path(
-        route = 'signup/', 
-        view = views.signup, 
-        name='signup'
-    ),
-    path(
-        route = 'me/profile/', 
-        view = views.update_profile, 
-        name='update'
     ),
 ]
