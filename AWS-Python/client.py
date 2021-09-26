11e796661cba22cb27cffb6ba40380140e6e7f0e
lotharking <andresfelipe083195@hotmail.com>
2021-09-26 15:58:15 +0000

    [change] Test

--- AWS-Python/client.py
+++ AWS-Python/client.py
@@ -6,4 +6,4 @@ response = client.list_objects(Bucket = 'aws-bma-test')
 
 for content in response['Contents']:
     obj_dict = client.get_object(Bucket='aws-bma-test', Key=content['Key'])
-    print(content['Key'], obj_dict['LastModified'])
+    print(content, ['Key'], obj_dict['LastModified'])
