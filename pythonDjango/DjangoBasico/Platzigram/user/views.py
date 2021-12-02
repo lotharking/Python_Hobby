fb9fa4b0160995220c72a1403d011875a68335c9
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-12-01 21:02:37 -0500

    Revert "[change] No message"
    This reverts commit 1f554e671d8e9ac3fae944eb3f3eae8ca69d3833.

--- pythonDjango/DjangoBasico/Platzigram/user/views.py
+++ pythonDjango/DjangoBasico/Platzigram/user/views.py
@@ -113,5 +113,9 @@ class DetailUsersView(LoginRequiredMixin, ListView):
         values = User.objects.values()
         df = pd.DataFrame.from_records(values)
         context['DataFrame'] = df
+        # for _,record in df.iterrows():
+        #     print(record.last_login)
+        # for record in df.iterrows():
+        #     print(record)
         return context
 
