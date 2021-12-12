7299037fd628dc999a25c43878f54331979a4885
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-12-12 16:40:28 -0500

    [create] create ajax

---
+++ pythonDjango/DjangoBasico/Platzigram/static/js/ajax.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#like_heart").on("click",function(e){
+        e.preventDefault();
+
+        // $ajax({
+        //     url: $(this).attr('href'),
+        //     type: $(this).attr('method'),
+        //     data: $(this).serializer(),
+
+        //     success: function(jason){
+        //         console.log(json)
+        //     }
+        // })
+    })
+})
