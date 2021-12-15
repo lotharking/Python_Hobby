59462fe1f0ac75a5ab2071878390d50644c0f554
Andres Vallecilla <andresfelipe083195@hotmail.com>
2021-12-14 21:01:04 -0500

    [change] example ajax

--- pythonDjango/DjangoBasico/Platzigram/static/js/ajax.js
+++ pythonDjango/DjangoBasico/Platzigram/static/js/ajax.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
     $("#like_heart").on("click",function(e){
         e.preventDefault();
+        
+        $.ajax({
+            type: 'get',
+            url: "{% url 'posts:likes2' post.pk %}",
+      
+            datatype:'json',
+            success: function(data){
+                      alert(n)
+                      console.log(data)
+                      console.log(n)
+                      },
+            error: console.log("SSS"),
+      
+          }); 
 
         // $ajax({
         //     url: $(this).attr('href'),
