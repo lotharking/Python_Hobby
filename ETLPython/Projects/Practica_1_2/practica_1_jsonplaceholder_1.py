4edf17ba3fe06e9e73e215bdbc8dd236eeea235c
Andres Vallecilla <andresfelipe083195@hotmail.com>
2022-11-10 20:24:47 -0500

    create schematic

--- ETLPython/Projects/Practica_1_2/practica_1_jsonplaceholder_1.py
+++ ETLPython/Projects/Practica_1_2/practica_1_jsonplaceholder_1.py
@@ -1,3 +1,19 @@
 import requests
 
 from prefect import task, Flow, flow
+
+@task
+def extract():
+    pass
+
+@task
+def load(response):
+    pass
+
+@flow
+def load_flow():
+    raw = extract()
+    load(raw)
+
+
+load_flow()
