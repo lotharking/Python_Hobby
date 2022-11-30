915b226fd32158484f9019b53fd1859dea11cd5c
Andres Vallecilla <andresfelipe083195@hotmail.com>
2022-11-29 21:20:23 -0500

    transform in flow

--- ETLPython/Projects/caso/flow_caso.py
+++ ETLPython/Projects/caso/flow_caso.py
@@ -1,7 +1,7 @@
 # Import libraries
 
 import pandas as pd
-import numpy as nd
+import numpy as np
 import yfinance as yf
 from datetime import date
 import requests
@@ -19,6 +19,7 @@ def extract(tickers, today):
         raw_df = pd.DataFrame(tk.history(period='1d'))
         raw_df.columns = raw_df.columns.str.lower()
         raw_df = raw_df[['open', 'high', 'low', 'close']]
+        raw_df.index = pd.to_datetime(raw_df.index, format='%m/%d/%Y').strftime('%Y-%m-%d')
 
         raw_dfs[ticker] = raw_df
 
@@ -35,10 +36,30 @@ def extract(tickers, today):
     return raw_dfs
 
 # Transform
-def transform(raw_dfs):
-    pass
+@task
+def transform(raw_dfs, tickers):
+    # Feature engineering
+    
+    for ticker in tickers:
+        df = raw_dfs[ticker]
+        
+        df['dif_open_close'] = df['open'] - df['close']
+        df['range_day'] = df['high'] - df['low']
+        df['signe_day'] = np.where(df['dif_open_close'] > 0.0, "+", np.where(df['dif_open_close'] < 0.0, "-", "0"))
+
+        df = df[["close", "dif_open_close", "range_day", "signe_day"]]
+        df.columns = map(lambda x: ticker + '_' + x, df.columns.to_list())
+
+        raw_dfs[ticker] = df
+
+    # Add
+    dfs_list = raw_dfs.values()
+    tablon = pd.concat(dfs_list, axis=1)
+
+    return tablon
 
 # Load
+@task
 def load():
     pass
 
@@ -49,8 +70,8 @@ with Flow("ETL caso") as flow:
     today = date.today()
     today = today.strftime("%Y-%m-%d")
 
-    raw_dfs = extract(tickers, today)
-    # tablon = transform(raw_dfs)
+    raw_dfs = extract(tickers=tickers, today=today)
+    transform(raw_dfs, tickers)
     # load(tablon)
 
 flow.run()
