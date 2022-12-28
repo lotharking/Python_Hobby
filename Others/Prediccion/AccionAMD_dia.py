012e7d47da882c63f105282c78b5a328084fdd31
Andres Vallecilla <andresfelipe083195@hotmail.com>
2022-12-28 00:39:54 -0500

    get day

--- Others/Prediccion/AccionAMD_dia.py
+++ Others/Prediccion/AccionAMD_dia.py
@@ -4,7 +4,7 @@ import yfinance as yf
 ticker = yf.Ticker("AMD")
 
 # Obtener el precio de cierre de la acción de AMD en el día de hoy
-history = ticker.history(interval="1d")
+history = ticker.history(period="1d")
 closing_price = history["Close"][0]
 
 print("El precio de cierre de la acción de AMD del día de hoy es", closing_price)
