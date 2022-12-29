5308334dd7902c9880498dab6f04e46e7393440c
Andres Vallecilla <andresfelipe083195@hotmail.com>
2022-12-28 20:20:36 -0500

    no message

--- Others/Prediccion/AccionAMD.py
+++ Others/Prediccion/AccionAMD.py
@@ -8,8 +8,7 @@ for root, dirs, files in os.walk("."):
     # Buscar el archivo "predictions.xlsx" en la lista de archivos de la carpeta actual
     if "predictions.xlsx" in files:
         # Si se encuentra el archivo, obtener la ruta completa del archivo
-        file_path = os.path.join(root, "predictions.xlsx")
-        
+        file_path = os.path.join(root, "predictions.xlsx")        
         # Abrir el archivo de Excel
         workbook = openpyxl.load_workbook(file_path)
         break
@@ -24,14 +23,12 @@ closing_prices = history["Close"]
 # Calcular el promedio del movimiento del precio de cierre en el último mes
 movement_average = sum(closing_prices) / len(closing_prices)
 
-# Obtener la fecha de mañana en formato "dd-mm-yyyy"
+# Obtener la fecha de hoy y mañana en formato "dd-mm-yyyy"
 tomorrow = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-# Obtener la fecha de hoy en formato "dd-mm-yyyy"
 today = datetime.datetime.today().strftime("%Y-%m-%d")
 
 # Seleccionar la primera hoja del libro
 worksheet = workbook.active
-
 # Obtener el número de filas en la hoja
 row_count = worksheet.max_row
 
@@ -42,7 +39,7 @@ for row in worksheet.iter_rows(min_row=2):
         # Verificar si el índice de tiempo (fecha) coincide con la fecha que se quiere buscar
         if index.strftime("%Y-%m-%d") == row[1].value:
             # Si la celda de la columna "Real" está vacía y es diferente al dia actual
-            if row[3].value is None and row[1].value != today:
+            if row[3].value is None and row[1].value != tomorrow:
                 row[3].value = check["Close"]
 
 # Añadir una nueva fila al final de la hoja
