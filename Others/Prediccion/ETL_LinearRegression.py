f9598bb26d952bb2a1a97f6df5be0b9ea1ea11e4
Andres Vallecilla <andresfelipe083195@hotmail.com>
2022-12-29 01:06:35 -0500

    adjust in linear regression

--- Others/Prediccion/ETL_LinearRegression.py
+++ Others/Prediccion/ETL_LinearRegression.py
@@ -5,19 +5,33 @@ import numpy as np
 import yfinance as yf
 import datetime
 from sklearn.linear_model import LinearRegression
+import csv
+import os
 
 from prefect import task, Flow
-from prefect.tasks.secrets import PrefectSecret
 
 # Extract of data
 @task
-def extract(ticker):
+def extract(ticker,file_path):
     # Obtener el precio de cierre de la acción de AMD en el último mes
     history = ticker.history(period="1mo")
 
+    # Abrir el archivo CSV en modo escritura
+    with open(file_path, 'w', newline='') as csvfile:
+        # Crear un objeto writer para escribir en el archivo CSV
+        writer = csv.writer(csvfile)
+        # Añadir una fila con los encabezados
+        writer.writerow(["Fecha", "Close", "Volume", "Market Index"])
+        
+        # Recorrer el DataFrame con los precios de cierre
+        for index, row in history.iterrows():
+            # Añadir una fila al archivo CSV con la fecha en formato "YYYY-MM-DD" y el precio de cierre
+            writer.writerow([index.strftime("%Y-%m-%d"), row["Close"], row["Volume"], row["Open"]])
+
 # Transform
 @task
-def transform(data):
+def transform(file_path):    
+    data = pd.read_csv(file_path)
     # Seleccionar la columna de "Close" como la variable dependiente (y)
     y = data["Close"]
 
@@ -43,11 +57,15 @@ with Flow("ETL caso") as flow:
     ticker = yf.Ticker("AMD")
     tomorrow = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
     today = datetime.datetime.today().strftime("%Y-%m-%d")
-    
-    data = pd.read_csv("./Others/Prediccion/dataframe.csv")
 
-    raw_dfs = extract(ticker)
-    # tablon = transform(data)
+    file_path = "./Others/Prediccion/dataframe.csv"
+
+    # Comprobar si el archivo existe
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+    raw_dfs = extract(ticker, file_path)
+    tablon = transform(file_path)
     # load(tablon, today, credentials)
 
 flow.run()
